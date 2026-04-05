3c1ba137d624d9d10630d032db17173feb08dbd1
trolldaddy <130296755+trolldaddy@users.noreply.github.com>
2026-04-06 06:02:36 +0800

    Update sw.js

--- static/sw.js
+++ static/sw.js
@@ -1,1 +1,7 @@
+// 這是最簡單的 Service Worker
+// 即使不處理離線快取，也必須監聽 fetch 事件，Chrome 才會顯示「安裝」按鈕
 
+self.addEventListener('fetch', function(event) {
+  // 這裡可以留空，或者讓請求正常通過
+  // 它的存在本身就是告訴瀏覽器：我有能力處理網路請求
+});
